bf19daaf3e2ba55028ed7f8217fb5084d0906839
JoTran <josephthientran@gmail.com>
2014-08-29 19:17:28 -0400

    Initial commit.

---
+++ script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	var cols = 10;
+	var rows = 10;
+	var board = $("#board");
+
+	setup(cols, rows, board);
+
+	board.on("mouseenter", ".square", function(){
+			$(this).css("background", "black");
+	});
+
+	$("#button").click(function(){
+		$(".square").css("background", "white");
+		var size = prompt("Enter new grid size (number of squares per row/column):");
+		while(board.width()/size < 3)
+			size = prompt("Squares would be too small. Enter valid grid size:");
+		setup(size, size, board);
+	});
+
+	function setup(cols, rows, board){
+		// clear board
+		board.empty();
+		// Add squares
+		for(var i = 0; i < cols; i++){
+			for(var j = 0; j < rows; j++){
+				board.append("<div class='square'></div>");
+			}
+		}
+		var width = board.width()/(cols);
+		var height = board.height()/(rows);
+		// Subtract 2 to accomodate for border and set sizes
+		$(".square").width(width-2);
+		$(".square").height(height-2);
+	};
+});
